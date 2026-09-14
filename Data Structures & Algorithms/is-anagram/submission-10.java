af16c9c54e7b11286540614dddfa9800570e2e45
Mohamed-Salad <124052952+Mohamed-Salad@users.noreply.github.com>
2026-09-14 13:11:45 +0100

    Update: is-anagram - submission-10

--- Data Structures & Algorithms/is-anagram/submission-10.java
+++ Data Structures & Algorithms/is-anagram/submission-10.java
@@ -1,48 +1,31 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //return sorting (s, t);
-        //return mapping(s,t);
-        return alphabetTable (s,t);
-
-    }
-    public boolean sorting (String s, String t){
-        if( s.length()!= t.length()){
-            return false;
-        }
-        char [] sSort = s.toCharArray();
-        char [] tSort = t.toCharArray(); 
-
-        Arrays.sort(sSort);
-        Arrays.sort(tSort);
-
-        return Arrays.equals(sSort, tSort);
-
-    }
-
-    public boolean mapping (String s, String t){
         if(s.length()!= t.length()){
             return false;
         }
-        HashMap<Character, Integer> countS = new HashMap <>();
-        HashMap<Character, Integer> countT = new HashMap <>();
-        for(int i = 0; i<s.length(); i++){
-            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i),0)+1);
-            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i),0)+1);
-        }
-        return countS.equals(countT);
+
+        return mapApproach(s,t);
+
     }
 
-    public boolean alphabetTable(String s, String t){
-        if(s.length()!=t.length()){
-            return false;
+
+    public boolean sortingApproach (String s, String t){
+        char []tSort = t.toCharArray();
+        char []sSort = s.toCharArray();
+        Arrays.sort(tSort);
+        Arrays.sort(sSort);
+        return Arrays.equals(tSort, sSort);
+    }
+
+    public boolean mapApproach (String s, String t){
+        HashMap<Character,Integer> countS = new HashMap<>();
+        HashMap<Character, Integer> countT = new HashMap<>();
+
+        for(int i =0; i<s.length();i++){
+            countS.put(s.charAt(i),countS.getOrDefault(s.charAt(i),0)+1);
+            countT.put(t.charAt(i),countT.getOrDefault(t.charAt(i),0)+1);
         }
-        int [] count = new int[26];
-        for(int i = 0;i<s.length();i++){
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
-        }
-        for(int val: count){
-            if (val!=0){return false;}
-        }
-    return true;}
+        return countT.equals(countS);
+    }
+
 }
