bb5f4323cce43fbf1352f520c4eb259f64500500
Mohamed-Salad <124052952+Mohamed-Salad@users.noreply.github.com>
2026-09-14 14:31:37 +0100

    Update: two-integer-sum - submission-17

--- Data Structures & Algorithms/two-integer-sum/submission-17.java
+++ Data Structures & Algorithms/two-integer-sum/submission-17.java
@@ -1,55 +1,33 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-      //  return bruteForce(nums, target);
-    //   return twoPointers (nums, target);
-    return twoPass(nums,target);
+        return mapApproach(nums,target);
     }
 
-    public int [] bruteForce (int [] nums, int target){
-        for (int i= 0; i<nums.length; i++){
+    public int [] bruteForce(int [] nums, int target){
+        for(int i = 0; i<nums.length; i++){
             for(int j = i+1 ; j<nums.length; j++){
-                if(nums[i] + nums [j] == target){
-                    return new int [] {i,j}; 
+                int sum = nums[i] + nums[j];
+                if (sum==target){
+                    return new int []   {i,j};
                 }
             }
         }
-    return new int []{};}
+    return new int []   {};}
 
+    public int [] mapApproach (int [] nums, int target ){
+        HashMap<Integer,Integer> contains = new HashMap<>();
 
-    public int [] twoPointers (int [] nums, int target){
-        int [][] pairs = new int [nums.length][2];
-        for (int i = 0; i<nums.length; i++){
-            pairs[i][0]= nums [i];
-            pairs[i][1] = i; 
-        }
-        Arrays.sort (pairs, Comparator.comparingInt(a-> a[0]));
-
-        int i = 0, j = nums.length - 1; 
-        while(i<j){
-            int cur = pairs[i][0]+ pairs[j][0];
-            if(cur== target){
-                return new int []{Math.min(pairs [i][1], pairs [j][1]),
-                                                                  Math.max(pairs[i][1], pairs[j][1])};
-            } else if (cur < target) {
-                i++;
-            } else {
-                j--;
+        for(int i =0; i<nums.length; i++){
+            int current = nums[i];
+            int wanted = target - current ; 
+            if(contains.containsKey(wanted)){
+                return new int [] { contains.get(wanted),i};
+            }
+            else{
+                contains.put(current,i);
             }
         }
-        return new int[0];
-    }
+    return new int [] {};}
 
-    public int [] twoPass(int [] nums, int target){
-        Map<Integer, Integer> indices = new HashMap <>();
-        for (int i = 0; i<nums.length; i++){
-            indices.put (nums[i],i);
-        }
-        for(int i = 0; i<nums.length; i++){
-            int diff = target - nums[i];
-            if (indices.containsKey(diff)&& indices.get(diff)!=i){
-                return new int [] {i, indices.get(diff)};
-            }
-        }
-    return new int [0];}
-    }
-  
+   
+}
