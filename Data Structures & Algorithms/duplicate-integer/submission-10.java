8eedbc3f9087a336952192e1d87876287eb691df
Mohamed-Salad <124052952+Mohamed-Salad@users.noreply.github.com>
2026-09-14 14:31:55 +0100

    Add: duplicate-integer - submission-10

---
+++ Data Structures & Algorithms/duplicate-integer/submission-10.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums){ 
+        return setApproach(nums);
+    }
+    public boolean sortingApproach (int [] nums){
+        Arrays.sort(nums);
+        for(int number=1;number<nums.length;number++){
+            if(nums[number]==nums[number -1]){
+                return true;
+            }
+        }
+    return false;}
+    public boolean bruteForceApproach (int [] nums){
+         for(int outer = 0; outer<nums.length;outer++){
+            for(int inner = outer +1; inner<nums.length; inner++){
+                if (nums[inner] == nums[outer] ){
+                    return true;
+                }
+           } 
+    }return false;}
+    public boolean mapMethod(int [] nums){
+        Map<Integer, Integer> storage = new HashMap<>();
+        for(int index = 0; index<nums.length; index++){
+            int value  = nums[index];
+            if(storage.containsKey(value)){
+                return true;
+            }
+            storage.put(value,index);
+        }
+    return false;}
+
+    public boolean setApproach(int [] nums){
+        Set<Integer> seen = new HashSet<>();
+        for(int values:nums){
+            if(seen.contains(values)){
+                return true;
+            }
+            seen.add(values);
+        }
+    return false;}
+}
+
+
