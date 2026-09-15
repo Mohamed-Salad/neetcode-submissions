7d6e3cbf02d08266fb08302f4295efc660938164
Mohamed-Salad <124052952+Mohamed-Salad@users.noreply.github.com>
2026-09-15 12:01:09 +0100

    Update: top-k-elements-in-list - submission-8

--- Data Structures & Algorithms/top-k-elements-in-list/submission-8.java
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap <Integer,Integer> frequency =  new HashMap<>();
-        for(int i = 0; i<nums.length;i++){
-            frequency.put(nums[i],frequency.getOrDefault(nums[i],0)+1);
-
+        HashMap<Integer,Integer> frequency = new HashMap <>();
+        for(int num : nums){
+            frequency.put(num, frequency.getOrDefault(num,0)+1);
         }
-        List<int []>arr = new ArrayList <>();
-        for(Map.Entry<Integer, Integer> entry : frequency.entrySet()){
-            arr.add(new int []{entry.getValue(), entry.getKey()});}
+        List<int []> arr = new ArrayList <>();
 
+        for(Map.Entry<Integer,Integer>entry : frequency.entrySet()){
+            arr.add(new int[]{entry.getValue(),entry.getKey()
+            });}
             arr.sort((a,b)-> b[0] - a[0]);
 
-            int [] res = new int [k];
-
-            for(int i = 0 ; i < k ; i++){
-                res [i] = arr.get (i)[1];
+            int [] answer= new int [k];
+            for(int i=0; i<k;i++){
+                answer [i] = arr.get(i)[1];
             }
-      return res;  }
-    }
+      return answer;  }
 
+    }
+   
